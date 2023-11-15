c14b7f1aa523f51db6847b9daeeb932505afd32c
Satyam Saxena <satyam20_ug@ee.nits.ac.in>
2023-11-16 00:09:05 +0530

    deployment fix

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const userRoutes = require("./userRoutes")
+require('dotenv').config()
 
 const app = express();
  
@@ -10,14 +11,13 @@ app.use(cors());
 app.use(express.json());
 app.use("/api/auth", userRoutes) //this means add this middleware with every request
 
-mongoose.connect(process.env.MONGO_URL)
+mongoose.connect('mongodb+srv://shubhsatya040302:C05xDNveL2Ai38Gg@cluster0.orwyan0.mongodb.net/language-learning?retryWrites=true&w=majority')
 .then( async ()=> {
     console.log("mongoose is connected");
     const db = await mongoose.connection.db;//this is to access the created database 
     const fetchEq = await db.collection("englishtohindi").find().toArray(); //this will get all the data inside easy_questions collections
     const fetchHq = await db.collection("hinditoenglish").find().toArray(); 
-
-    global.EngtoHinQuestions = fetchEq; //now E_questions is a global variable
+    global.EngtoHinQuestions = fetchEq; //now E_questions is a global variable 
     global.HintoEngQuestions = fetchHq; //now E_questions is a global variable
 
     
